0c51877ef74aecc13ad08d02836c56c4515ad30c
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-11 15:37:44 -0300

    Header da aplicação

--- rentx-mobile/src/screens/Home/index.tsx
+++ rentx-mobile/src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 import Logo from '../../assets/logo.svg';
-import { Container, Header } from './styles';
+import { Container, Header, HeaderContent, TotalCar } from './styles';
 
 export function Home() {
   return (
@@ -12,8 +13,16 @@ export function Home() {
       />
 
       <Header>
-        <Logo />
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCar>
+            Total de 12 Carros
+          </TotalCar>
+        </HeaderContent>
       </Header>
-    </Container>
+    </Container >
   );
 }
