5d94b0599b1abe19e33383b294b7f4965a363eae
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-13 11:32:13 -0300

    Header

--- rentx-mobile/src/screens/Home/index.tsx
+++ rentx-mobile/src/screens/Home/index.tsx
@@ -1,11 +1,19 @@
 import { StatusBar, Text } from "react-native"
-import { Header, HomeContainer } from "./styles"
+import { RFValue } from "react-native-responsive-fontsize"
+import { Header, HeaderContent, HomeContainer, TotalCar } from "./styles"
+
+import Logo from "../../assets/logo.svg"
 
 export function Home() {
   return (
     <HomeContainer>
-      <StatusBar translucent barStyle="dark-content" backgroundColor="transparent" />
-      <Header />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCar>Total de 12 Carros</TotalCar>
+        </HeaderContent>
+      </Header>
       <Text>Home</Text>
     </HomeContainer>
   )
--- rentx-mobile/src/screens/Home/styles.ts
+++ rentx-mobile/src/screens/Home/styles.ts
@@ -1,3 +1,4 @@
+import { RFValue } from "react-native-responsive-fontsize"
 import styled from "styled-components/native"
 
 export const HomeContainer = styled.View`
@@ -10,5 +11,21 @@ export const Header = styled.View`
   width: 100%;
   height: 113px;
 
+  background: ${({ theme }) => theme.colors.header};
+
   font-family: ${({ theme }) => theme.fonts.secondary_600};
+  justify-content: flex-end;
+
+  padding: 32px 24px;
+`
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const TotalCar = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `
