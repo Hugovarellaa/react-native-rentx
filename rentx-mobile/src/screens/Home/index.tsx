71cc6fef6b35cd760bd7ca6e6c4ae22a9ea43695
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-13 11:03:34 -0300

    SVG e PNG

--- rentx-mobile/src/screens/Home/index.tsx
+++ rentx-mobile/src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
-import { Text } from "react-native"
-import { HomeContainer } from "./styles"
+import { StatusBar, Text } from "react-native"
+import { Header, HomeContainer } from "./styles"
 
 export function Home() {
   return (
     <HomeContainer>
+      <StatusBar translucent barStyle="dark-content" backgroundColor="transparent" />
+      <Header />
       <Text>Home</Text>
     </HomeContainer>
   )
--- rentx-mobile/src/screens/Home/styles.ts
+++ rentx-mobile/src/screens/Home/styles.ts
@@ -2,6 +2,13 @@ import styled from "styled-components/native"
 
 export const HomeContainer = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
+
+  background: ${({ theme }) => theme.colors.background_primary};
+`
+
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
 `
