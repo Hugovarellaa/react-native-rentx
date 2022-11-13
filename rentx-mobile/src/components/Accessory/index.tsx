a30c9c53e45f4629d6001bf0ff45727e48ec0773
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-13 15:19:05 -0300

    Component Accessory

--- rentx-mobile/src/components/Accessory/index.tsx
+++ rentx-mobile/src/components/Accessory/index.tsx
@@ -1,3 +1,17 @@
-export function Accessory() {
-  return <AccessoryContainer />
+import React from "react"
+import { SvgProps } from "react-native-svg"
+import { AccessoryContainer, Name } from "./styles"
+
+interface Props {
+  name: string
+  icon: React.FC<SvgProps>
+}
+
+export function Accessory({ name, icon: Icon }: Props) {
+  return (
+    <AccessoryContainer>
+      <Icon width={32} height={32} />
+      <Name>{name}</Name>
+    </AccessoryContainer>
+  )
 }
