14ee77eb635844364e5a543518b04885641b2279
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-14 09:44:29 -0300

    Configurando dias e meses do calendarios

--- rentx-mobile/src/components/Calendar/index.tsx
+++ rentx-mobile/src/components/Calendar/index.tsx
@@ -1,6 +1,16 @@
 import { Feather } from "@expo/vector-icons"
-import { Calendar as CustomCalendar } from "react-native-calendars"
+import { Calendar as CustomCalendar, LocaleConfig } from "react-native-calendars"
 import { useTheme } from "styled-components"
+
+LocaleConfig.locales['pt-br'] = {
+  monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+  monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta' , 'Quinta', 'Sexta', 'Sábado', 'Domingo'],
+  dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA' , 'QUI', 'SEX', 'SAB', 'DOM'],
+  today: 'Hoje'
+}
+
+LocaleConfig.defaultLocale = 'pt-br'
 
 export function Calendar() {
   const theme = useTheme()
@@ -15,6 +25,14 @@ export function Calendar() {
         borderBottomColor: theme.colors.text_detail,
         paddingBottom: 10,
         marginBottom: 10
+      }}
+      theme={{
+        textDayFontFamily: theme.fonts.primary_400,
+        textDayHeaderFontFamily: theme.fonts.primary_500,
+        textDayHeaderFontSize: 10,
+        arrowStyle: {
+          marginHorizontal: -15
+        }
       }}
     />
   )
