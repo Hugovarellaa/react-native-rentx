0ec048e0d26f874651ed95c6b99bb906c9b22cdf
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-16 13:11:20 -0300

    Install expo-splash-screen

--- rentx-mobile/src/screens/Splash/index.tsx
+++ rentx-mobile/src/screens/Splash/index.tsx
@@ -1,12 +1,49 @@
-import { StatusBar } from "react-native";
+import { Button, Dimensions, StatusBar, StyleSheet } from "react-native";
 import { SplashContainer } from "./styles";
 
-export function Splash (){
+import Animated, {
+  Easing, useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from "react-native-reanimated";
+
+const width = Dimensions.get('window').width
+
+export function Splash() {
+  const animation = useSharedValue(0)
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 3000,
+            easing: Easing.bezier(.37,.86,.13,.01)
+          })
+        }
+      ]
+    }
+  })
+
+  function handleAnimationPosition(){
+    animation.value = Math.random() * (width - 100)
+  }
+
   return (
     <SplashContainer>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
 
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <Button title="Move" onPress={handleAnimationPosition} />
 
     </SplashContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red'
+  }
+})
