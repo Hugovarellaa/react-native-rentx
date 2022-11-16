b274bbd9c3407c5bea4b7336c8954f135266cb76
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-16 15:53:57 -0300

    Redirecionando para a interface Home depois da splash animada

--- rentx-mobile/src/screens/Splash/index.tsx
+++ rentx-mobile/src/screens/Splash/index.tsx
@@ -1,12 +1,17 @@
+import { NavigationProp, ParamListBase, useNavigation } from "@react-navigation/native";
 import { useEffect } from "react";
 import { StatusBar } from "react-native";
-import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import Animated, { Extrapolate, interpolate, runOnJS, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 import BrandSvg from '../../assets/brand.svg';
 import LogoSvg from '../../assets/logo.svg';
 import { SplashContainer } from "./styles";
 
+
+
 export function Splash() {
   const splashAnimation = useSharedValue(0)
+  const navigation = useNavigation<NavigationProp<ParamListBase>>();
+
 
   const brandStyle = useAnimatedStyle(() => {
     return {
@@ -38,19 +43,30 @@ export function Splash() {
     }
   })
 
+  function startApp() {
+    navigation.navigate('Home')
+  }
+
   useEffect(() => {
-    splashAnimation.value = withTiming(50, { duration: 4000 })
-  }, [])
+    splashAnimation.value = withTiming(
+      50,
+      { duration: 1000 },
+      () => {
+        'worklet'
+        runOnJS(startApp)()
+      }
+    )
+  }, [splashAnimation])
 
   return (
     <SplashContainer>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
 
-      <Animated.View style={[brandStyle, {position: 'absolute'}]}>
+      <Animated.View style={[brandStyle, { position: 'absolute' }]}>
         <BrandSvg width={80} height={50} />
       </Animated.View>
 
-      <Animated.View style={[logoStyle, {position: 'absolute'}]}>
+      <Animated.View style={[logoStyle, { position: 'absolute' }]}>
         <LogoSvg width={180} height={20} />
       </Animated.View>
 
