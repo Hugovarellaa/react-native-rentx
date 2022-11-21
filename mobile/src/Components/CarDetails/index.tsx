16b553067801b517c4c39c856cfd6cedb399fb1c
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-21 16:53:22 -0300

    Component CarDetails

---
+++ mobile/src/Components/CarDetails/index.tsx
@@ -0,0 +1,11 @@
+import { CarDetailsContainer, Header } from "./styles";
+
+export function CarDetails() {
+  return (
+    <CarDetailsContainer>
+      <Header>
+
+      </Header>
+    </CarDetailsContainer>
+  )
+}
