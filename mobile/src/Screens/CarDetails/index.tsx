05c9b6ca8c0d937e0729855879552fdd1bf7a1ee
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-21 18:04:19 -0300

    Interface de DetailsCar

--- mobile/src/Screens/CarDetails/index.tsx
+++ mobile/src/Screens/CarDetails/index.tsx
@@ -1,6 +1,6 @@
 import { BackButton } from "../../Components/BackButton";
 import { ImageSlider } from "../../Components/ImageSlider";
-import { CarDetailsContainer, CarImages, Header } from "./styles";
+import { About, Brand, CarDetailsContainer, CarImages, Content, Description, Details, Header, Name, Period, Price, Rent } from "./styles";
 
 export function CarDetails() {
   return (
@@ -13,6 +13,24 @@ export function CarDetails() {
           imagesUrl={["https://www.pngarts.com/files/3/Lamborghini-Huracan-PNG-Download-Image.png"]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+        Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
 
     </CarDetailsContainer>
   )
