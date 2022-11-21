1d48e916094cf01c6ab3a067934429a693d905df
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-21 16:28:10 -0300

    Component de Carro

---
+++ mobile/src/Components/Car/inde.tsx
@@ -0,0 +1,40 @@
+import { About, Brand, CarContainer, CarImage, Details, Name, Period, Price, Rent, Type } from "./styles";
+
+import GasolineSvg from "../../assets/gasoline.svg";
+
+interface Car {
+  brand: string
+  name: string;
+  rent: {
+    period: string
+    price: number
+  }
+  thumbnail: string
+}
+
+interface CarProps {
+  data: Car
+}
+
+export function Car({ data }: CarProps) {
+  return (
+    <CarContainer>
+      <Details>
+        <Brand>{data.brand}</Brand>
+        <Name>{data.name}</Name>
+
+        <About>
+          <Rent>
+            <Period>{data.rent.period}</Period>
+            <Price>R$ {data.rent.price}</Price>
+          </Rent>
+          <Type>
+            <GasolineSvg />
+          </Type>
+        </About>
+
+      </Details>
+      <CarImage source={{ uri: data.thumbnail }} />
+    </CarContainer>
+  )
+}
--- mobile/src/Screens/Home/index.tsx
+++ mobile/src/Screens/Home/index.tsx
@@ -1,9 +1,22 @@
 import { StatusBar } from "react-native";
 import { RFValue } from 'react-native-responsive-fontsize';
 import LogoSvg from '../../assets/logo.svg';
+import { Car } from "../../Components/Car/inde";
 import { Header, HeaderWrapper, HomeContainer, TotalCar } from "./styles";
 
+// ''
+
 export function Home() {
+  const carOne = {
+    brand: 'Lamborghini',
+    name: 'Huracan',
+    rent: {
+      period: 'Ao dia',
+      price: 580
+    },
+    thumbnail: 'https://www.pngarts.com/files/3/Lamborghini-Huracan-PNG-Download-Image.png'
+  }
+
   return (
     <HomeContainer>
       <StatusBar
@@ -11,12 +24,18 @@ export function Home() {
         translucent
         barStyle="light-content"
       />
+
       <Header>
         <HeaderWrapper>
           <LogoSvg width={RFValue(108)} height={RFValue(12)} />
           <TotalCar>Total de 12 Carros</TotalCar>
         </HeaderWrapper>
       </Header>
+
+      <Car data={carOne} />
+      <Car data={carOne} />
+      <Car data={carOne} />
+      <Car data={carOne} />
     </HomeContainer>
   )
 }
