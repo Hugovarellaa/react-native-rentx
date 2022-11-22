bd30c6981a430f881e5857c1c4053509327d85bf
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-22 14:34:51 -0300

    Icone de mudanças de meses

--- mobile/src/Components/Calendar/index.tsx
+++ mobile/src/Components/Calendar/index.tsx
@@ -1,7 +1,14 @@
+import { Feather } from '@expo/vector-icons'
 import { Calendar as CustomCalendar } from 'react-native-calendars'
+import { useTheme } from 'styled-components'
 
 export function Calendar() {
+  const theme = useTheme()
   return (
-    <CustomCalendar />
+    <CustomCalendar
+      renderArrow={(direction) =>
+        <Feather size={24} color={theme.colors.text} name={direction === 'left' ? 'chevron-left' : 'chevron-right'} />
+      }
+    />
   )
 }
