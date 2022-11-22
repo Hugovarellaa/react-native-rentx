70ad86839f17063984db69c30608aab129c9b044
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-21 21:33:25 -0300

    Interface Scheduling

---
+++ mobile/src/Screens/Scheduling/index.tsx
@@ -0,0 +1,48 @@
+import { BackButton } from "../../Components/BackButton";
+import { Content, DateInfo, DateTitle, DateValue, Footer, Header, RentalPeriod, SchedulingContainer, Title } from "./styles";
+
+import { StatusBar } from "react-native";
+import ArrowLeftSvg from '../../assets/arrow.svg';
+import { Button } from "../../Components/Button";
+
+export function Scheduling() {
+  return (
+    <SchedulingContainer>
+      <StatusBar
+        backgroundColor='transparent'
+        translucent
+        barStyle="light-content"
+      />
+      <Header>
+        <BackButton color="white" />
+        <Title>
+          Escolha uma{'\n'}
+          data de início e{'\n'}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>22/10/2022</DateValue>
+          </DateInfo>
+
+          <ArrowLeftSvg />
+
+          <DateInfo>
+            <DateTitle>ATE</DateTitle>
+            <DateValue selected>22/10/2022</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+
+      </Content>
+
+      <Footer>
+        <Button title="Confirma" onPress={() => { }} />
+      </Footer>
+    </SchedulingContainer>
+  )
+}
