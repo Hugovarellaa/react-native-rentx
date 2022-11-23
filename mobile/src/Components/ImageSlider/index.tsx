3f7715e001ea184bc30631fe368e4ea67363d2f7
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-23 16:27:14 -0300

    Slider de carros em CarDetails

--- mobile/src/Components/ImageSlider/index.tsx
+++ mobile/src/Components/ImageSlider/index.tsx
@@ -1,3 +1,4 @@
+import { FlatList } from "react-native";
 import { CarImage, CarImageWrapper, ImageIndex, ImageIndexes, ImageSliderContainer } from "./styles";
 
 interface Props {
@@ -8,18 +9,25 @@ export function ImageSlider({ imagesUrl }: Props) {
   return (
     <ImageSliderContainer>
       <ImageIndexes>
-        <ImageIndex active={false} />
-        <ImageIndex active={true} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
+        {
+          imagesUrl.map((_, index) => (
+            <ImageIndex key={String(index)} active={true} />
+          ))
+        }
       </ImageIndexes>
 
-      <CarImageWrapper>
-        <CarImage
-          source={{ uri: imagesUrl[0] }}
-          resizeMode='contain'
-        />
-      </CarImageWrapper>
+      <FlatList
+        data={imagesUrl}
+        keyExtractor={key => key}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        renderItem={({ item }) => (
+          <CarImageWrapper>
+            <CarImage source={{ uri: item }} resizeMode='contain' />
+          </CarImageWrapper>
+        )}
+      />
+
     </ImageSliderContainer>
   )
 }
