b696982393501d879077219957e656581c1ae4a3
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-23 16:45:38 -0300

    Obtendo a imagem em foco

--- mobile/src/Components/ImageSlider/index.tsx
+++ mobile/src/Components/ImageSlider/index.tsx
@@ -1,17 +1,30 @@
-import { FlatList } from "react-native";
+import { useRef, useState } from "react";
+import { FlatList, ViewToken } from "react-native";
 import { CarImage, CarImageWrapper, ImageIndex, ImageIndexes, ImageSliderContainer } from "./styles";
 
 interface Props {
   imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export function ImageSlider({ imagesUrl }: Props) {
+  const [imageIndex, setImageIndex] = useState(0)
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    const index = info.viewableItems[0].index!
+    setImageIndex(index)
+  })
+
   return (
     <ImageSliderContainer>
       <ImageIndexes>
         {
           imagesUrl.map((_, index) => (
-            <ImageIndex key={String(index)} active={true} />
+            <ImageIndex key={String(index)} active={index === imageIndex} />
           ))
         }
       </ImageIndexes>
@@ -21,6 +34,7 @@ export function ImageSlider({ imagesUrl }: Props) {
         keyExtractor={key => key}
         showsHorizontalScrollIndicator={false}
         horizontal
+        onViewableItemsChanged={indexChanged.current}
         renderItem={({ item }) => (
           <CarImageWrapper>
             <CarImage source={{ uri: item }} resizeMode='contain' />
