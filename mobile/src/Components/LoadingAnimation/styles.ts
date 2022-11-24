b03428376e5352c07e1e49ae54501b026ad7573d
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-23 23:15:35 -0300

    Usando o lottie animação de carregamento

---
+++ mobile/src/Components/LoadingAnimation/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+export const LoadingAnimationContainer = styled.View`
+  flex: 1;
+  align-self: center;
+  justify-content: center;
+`
--- mobile/src/Screens/SchedulingCompleted/index.tsx
+++ mobile/src/Screens/SchedulingCompleted/index.tsx
@@ -1,18 +1,25 @@
 import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { StatusBar, useWindowDimensions } from 'react-native';
+import { RootStackParamList } from "../../@types/navigation";
 import DoneSvg from '../../assets/done.svg';
 import LogoSvg from "../../assets/logo_background_gray.svg";
 import { ButtonConfirm } from '../../Components/ButtonConfirm';
 import { Content, Footer, SchedulingCompletedContainer, SubTitle, Title } from "./styles";
 
 
+type SchedulingScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'SchedulingCompleted'
+>;
+
 export function SchedulingCompleted() {
   const { width } = useWindowDimensions()
 
-  const navigation = useNavigation()
+  const { navigate } = useNavigation<SchedulingScreenNavigationProp>()
 
   function handleNextPage() {
-    navigation.navigate('Home')
+    navigate('Home')
   }
   return (
     <SchedulingCompletedContainer>
